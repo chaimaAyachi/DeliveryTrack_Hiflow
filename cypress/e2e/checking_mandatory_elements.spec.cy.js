bf81bbdd419db35fb4f6078a3a292ac0988c1ea7
chaimaAyachi <ayashichayma@gmail.com>
2024-02-26 22:54:05 +0100

    check page elements

--- cypress/e2e/checking_mandatory_elements.spec.cy.js
+++ cypress/e2e/checking_mandatory_elements.spec.cy.js
@@ -1,16 +1,17 @@
-const { beforeEach } = require("mocha")
-const cypress = require("cypress")
-
-
-let transalation= cy.fixture('fixtures/fr.json')
-
-
 
 describe('template spec', () => {
+
+  let translation;
+
+
   beforeEach(() => {
+
+    //Translation
+    cy.fixture('fr').then(fixture =>{translation=fixture})
+
     // Avant chaque test, visiter cette URL
     //cy.visit('https://test-track.hiflow.com/fr/05D2E6D41D54CEA');
-    cy.request({
+    cy.visit({
       url: 'https://test-track.hiflow.com/fr/05D2E6D41D54CEA', // Remplacez par l'URL de votre choix
       headers: {
         'Accept-Language': 'fr;q=1,en;q=0.9,es;q=0.8,de;q=0.7,it;q=0.6,nl;q=0.5,pt;q=0.4'
@@ -21,16 +22,13 @@ describe('template spec', () => {
   it('checking elements', () => {
     //Client name should exist
     cy.get('.heading-1.mr-3').should('exist')
-    //Langage icon should exist
-    .and('id=headlessui-menu-button-:R1j6:').should('exist')
     //"Your transfer" title and section should exist
-    .and('h1[class='heading-1']').should('exist')
-    .and('.mt-24.w-full.p-9.border-0.bg-white.rounded-md.mb-16.relative.flex.align-center.items-center').should('exist')
+    cy.get('.py-64 > .heading-1').should('exist')
+    cy.get('.mt-24.w-full.p-9.border-0.bg-white.rounded-md.mb-16.relative.flex.align-center.items-center').should('exist')
     //3Tabs:Suivi, Détail de transport and Documents should exist (default langage: French)
     cy.contains(translation.Tracking).should('exist')
     cy.contains(translation.Details).should('exist')
     cy.contains(translation.Documents).should('exist')
-
     //Top of the page button shoul exist
     cy.contains(translation.topOfpage).should('exist')
     
